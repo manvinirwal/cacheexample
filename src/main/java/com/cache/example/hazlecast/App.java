225fd3e5f862fa5099f8fb37c5834bf92a26dedc
Manvi Nirwal <manvi.nirwal@publicissapient.com>
2020-01-11 23:54:34 +0530

    first commit

---
+++ src/main/java/com/cache/example/hazlecast/App.java
@@ -0,0 +1,13 @@
+package com.cache.example.hazlecast;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
